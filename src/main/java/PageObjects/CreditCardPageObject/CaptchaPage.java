7365dda7a2d5a9256929527607f66c9abfafbb3a
ab020xd <Kontrol@22>
2022-07-20 10:05:55 +0200

    package update

--- src/main/java/PageObjects/CreditCardPageObject/CaptchaPage.java
+++ src/main/java/PageObjects/CreditCardPageObject/CaptchaPage.java
@@ -5,10 +5,6 @@ import Utilities.SeleniumDriver;
 import Utilities.TestReporter;
 import org.openqa.selenium.By;
 
-import org.bytedeco.javacpp.*;
-import static org.bytedeco.javacpp.lept.*;
-import static org.bytedeco.javacpp.tesseract.*;
-
 import static Utilities.SeleniumDriver.driver;
 
 public class CaptchaPage {
@@ -47,29 +43,6 @@ public class CaptchaPage {
         }
 
 
-    }
-
-    public String ReadTextFromImage(){
-        BytePointer outText;
-        TessBaseAPI api = new TessBaseAPI();
-        // Initialize tesseract-ocr with English, without specifying tessdata path
-        if (api.Init(null, "eng") != 0) {
-            System.err.println("Could not initialize tesseract.");
-            System.exit(1);
-        }
-
-        // Open input image with leptonica library
-        PIX image = pixRead( "CC/CaptchImage/captch1.tiff");
-        api.SetImage(image);
-        // Get OCR result
-        outText = api.GetUTF8Text();
-        System.out.println("OCR output:\n" + outText.getString());
-
-        // Destroy used object and release memory
-        api.End();
-        outText.deallocate();
-        pixDestroy(image);
-        return  outText.getString();
     }
 
     public void captchaExistingApplication(String idNumber, String refNumber,String identificationType) throws Exception {
