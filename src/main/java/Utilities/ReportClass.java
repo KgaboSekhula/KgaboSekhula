d0bdfd62c42e839912c96a850acd0cf13f660267
ab020xd <Kontrol@22>
2022-07-20 10:08:51 +0200

    logger update

--- src/main/java/Utilities/ReportClass.java
+++ src/main/java/Utilities/ReportClass.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 
-import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+
 
 public class ReportClass {
 
@@ -20,18 +20,18 @@ public class ReportClass {
         //check if the folder doesn't already exist on the file system.
         //If the folder already exists, do nothing, else create the folder.
         if (!rootReportFolder.exists()) {
-            LOGGER.info("creating directory: " + rootReportFolder);
+           // LOGGER.info("creating directory: " + rootReportFolder);
             try {
                 rootReportFolder.mkdir();
             } catch (Exception e) {
-                LOGGER.info("Failed to create directory" + rootReportFolder);
+                //LOGGER.info("Failed to create directory" + rootReportFolder);
             }
         }
 
         // Root Test Suite Folder [Example : Home Loans.]
         File testFolder = new File( rootReportFolder + File.separator);
         if (!testFolder.exists()) {
-            LOGGER.info("Creating Site directory" + testFolder);
+           // LOGGER.info("Creating Site directory" + testFolder);
             testFolder.mkdir();
         }
 
@@ -40,7 +40,7 @@ public class ReportClass {
         String reportFolderName = "Automated Test Execution Report_" + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
         File reportFolder = new File(testFolder.getAbsolutePath() + File.separator + reportFolderName);
         //Root Test Report Folder  [Example : Search HomeLoans Application Test.]
-        LOGGER.info("creating directory:" + reportFolder );
+      //  LOGGER.info("creating directory:" + reportFolder );
         reportFolder.mkdir();
 
         File htmlTemplateFile = new File(reportFolder+File.separator+"MyReport.html");
@@ -48,7 +48,7 @@ public class ReportClass {
         fullReportPath = reportFolder+File.separator+"MyReport.html";
         File screenShortFolder = new File( reportFolder+ File.separator + "Screenshots");
         if (!screenShortFolder.exists()) {
-            LOGGER.info("creating directory: " + screenShortFolder);
+           // LOGGER.info("creating directory: " + screenShortFolder);
             screenShortFolder.mkdir();
             screenshortReportPath =screenShortFolder.getAbsolutePath();
         }
